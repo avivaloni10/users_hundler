b8f6ddd39e8ea0336a64b93f064a534cace46fbc
Aviv Aloni <aviv@vulcan.io>
2023-02-28 01:53:47 +0200

    Add basic integration tests

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
 from routers import users_router
-from config import engine
+from config.db import engine
 from models import user
 
 user.Base.metadata.create_all(bind=engine)
@@ -14,8 +14,8 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/ping")
+async def say_hello():
+    return {"pong"}
 
-app.include_router(router.router, prefix="/users", tags=["users"])
+app.include_router(users_router.router, prefix="/users", tags=["users"])
--- routers/users_router.py
+++ routers/users_router.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends
-from config import SessionLocal
+from config.db import SessionLocal
 from sqlalchemy.orm import Session
-from schemas import RequestUser, Response
-from dal import user
+from schemas.user_schemas import RequestUser, Response
+from dal import user as user_crud
 
 router = APIRouter()
 
@@ -17,30 +17,30 @@ def get_db():
 
 @router.post("/")
 async def create(request: RequestUser, db: Session = Depends(get_db)):
-    crud.create_user(db, request.parameter)
+    user_crud.create_user(db, request.parameter)
     return Response(code=200, status="OK", message="User created successfully").dict(exclude_none=True)
 
 
 @router.get("/")
 async def get(db: Session = Depends(get_db)):
-    user = crud.get_users(db, 0, 100)
-    return Response(code=200, status="OK", message="Users batch fetched successfully", result=user).dict(
+    users = user_crud.get_users(db, 0, 100)
+    return Response(code=200, status="OK", message="Users batch fetched successfully", result=users).dict(
         exclude_none=True)
 
 
 @router.get("/{email}")
 async def get_by_email(email: str, db: Session = Depends(get_db)):
-    user = crud.get_user_by_email(db, email)
+    user = user_crud.get_user_by_email(db, email)
     return Response(code=200, status="OK", message="User fetched successfully", result=user).dict(exclude_none=True)
 
 
 @router.put("/{email}")
 async def update_by_email(email: str, request: RequestUser, db: Session = Depends(get_db)):
-    user = crud.update_user_by_email(db, email, request.parameter)
+    user = user_crud.update_user_by_email(db, email, request.parameter)
     return Response(code=200, status="OK", message="User updated successfully", result=user).dict(exclude_none=True)
 
 
 @router.delete("/{email}")
-async def update_by_email(email: str, db: Session = Depends(get_db)):
-    crud.delete_user_by_email(db, email)
+async def delete_by_email(email: str, db: Session = Depends(get_db)):
+    user_crud.delete_user_by_email(db, email)
     return Response(code=200, status="OK", message="User deleted successfully").dict(exclude_none=True)
